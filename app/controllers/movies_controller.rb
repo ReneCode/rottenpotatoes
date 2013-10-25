8af718b7ce8dcfc550188c00d7edcb42663f1973
= <saasbook@saasbook.(none)>
2013-10-25 11:01:06 +0200

    hw3-part3

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,11 +7,29 @@ class MoviesController < ApplicationController
   end
 
   def index
+    # get sort order from the url-parameter
     order_field = params[:order]
-    # ratings = { "P"=>1, "G"=1} .keys
-    ratings = params[:ratings]
-    @checked_ratings = Movie.valid_ratings
-    @checked_ratings = ratings.keys if (ratings != nil)
+    if order_field == nil
+      # if no order set, get the last order from the session
+      order_field = session[:order] 
+    else
+      # store order into the session for the next time
+      session[:order] = order_field
+    end
+
+    # ratings = { "P"=>1, "G"=1}
+    # get only the keys of the params
+    @checked_ratings = params[:ratings].keys if (params[:ratings] != nil)
+    if @checked_ratings == nil
+      # if not set via URL, than get stored ratings
+      @checked_ratings = session[:ratings]
+      # all ratingss on, if there is no rating stored
+      @checked_ratings = Movie.valid_ratings if @checked_ratings == nil
+    else
+      # store ratings for next call
+      session[:ratings] = @checked_ratings
+    end
+    # query to database
     @movies = Movie.where(rating: @checked_ratings).all(:order => order_field)
     @css_title = 'hilite' if order_field =~ /title/
     @css_release_date = 'hilite' if order_field =~ /release_date/
